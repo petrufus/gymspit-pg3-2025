59e70c1ee18f97dbed28e6c068f2689b587545b2
petrufus <petr.sperk@student.gymspit.cz>
2025-09-24 09:38:53 +0200

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿void FizzBuzz(int number)
+{
+    if ((number % 3 != 0 && number % 5 != 0) | number == 0)
+    {
+        Console.WriteLine(number);
+    }
+    else if (number % 3 == 0 && number % 5 != 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (number % 3 != 0 && number % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if (number % 3 == 0 && number % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+}
+
+
+
+for (int i = 0; i <= 100; i++)
+{
+    FizzBuzz(i);
+}
+
+
+Console.WriteLine("input number to check if it Fizzles or buzzles fr");
+
+while (true)
+{
+    string wr = Console.ReadLine();
+    int.TryParse(wr, out int number);
+    FizzBuzz(number);
+}
+
